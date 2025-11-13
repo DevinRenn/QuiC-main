66c556dfca7eb9261603b4f051a37f85be8ea885
zbolon <zachbolon@comcast.net>
2025-11-13 11:54:40 -0700

    Revert "Almost done with home page"
    This reverts commit 0da425982b4335a023d6666dec91a30440abdbb5.

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -19,9 +19,8 @@ CREATE TABLE IF NOT EXISTS users_to_folders (
 CREATE TABLE IF NOT EXISTS sets (
     set_id SERIAL PRIMARY KEY,
     set_name VARCHAR(100) NOT NULL,
-    set_description VARCHAR(300) NOT NULL
+    set_description VARCHAR(300)
 );
-
 
 CREATE TABLE IF NOT EXISTS folders_to_sets (
     folder_id INT REFERENCES folders(folder_id) ON DELETE CASCADE,
